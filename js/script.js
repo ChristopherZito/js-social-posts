d0071ea1a94074043222cc1b33b260e0b1674e88
christopherzito <christopherzito11@gmail.com>
2021-11-05 13:40:13 +0100

    upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Creiamo il nostro array di oggetti che rappresentano ciascun post.
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+// nome autore,
+// foto profilo,
+// data,
+// testo del post,
+// immagine (non tutti i post devono avere una immagine) [quindi gestisco il caso],
+// numero di likes.
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+// - Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
